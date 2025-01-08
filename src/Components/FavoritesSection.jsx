fd9570c954991fe23b494995e12ce06d86b9ebef
WillerLucoles <willerlob@gmail.com>
2025-01-07 22:56:05 -0300

    feat: add react-icons dependency; update FavoritesSection styling and structure

--- src/Components/FavoritesSection.jsx
+++ src/Components/FavoritesSection.jsx
@@ -1,34 +1,29 @@
-import { useState } from "react";
 import { useFavorites } from "../Context/FavoritesContext";
 
 const FavoritesSection = () => {
-  const [isOpen, setIsOpen] = useState(true);
   const { favorites } = useFavorites();
 
   return (
     <section className="mb-6">
-      <button
-        onClick={() => setIsOpen(!isOpen)}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        {isOpen ? "Ocultar Favoritos" : "Exibir Favoritos"}
-      </button>
-      {isOpen && (
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mt-4">
-          {favorites.length === 0 ? (
-            <p>Nenhum favorito ainda.</p>
-          ) : (
-            favorites.map((image) => (
-              <img
-                key={image.id}
-                src={image.download_url}
-                alt={image.author}
-                className="rounded shadow-lg"
-              />
-            ))
-          )}
-        </div>
-      )}
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold text-gray-800">Favoritos</h2>
+        <div className="border-b border-gray-300 mt-1"></div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
+        {favorites.length === 0 ? (
+          <p className="text-gray-600 text-center">Nenhum favorito ainda.</p>
+        ) : (
+          favorites.map((image) => (
+            <img
+              key={image.id}
+              src={image.download_url}
+              alt={image.author}
+              className="rounded shadow-md"
+            />
+          ))
+        )}
+      </div>
     </section>
   );
 };
